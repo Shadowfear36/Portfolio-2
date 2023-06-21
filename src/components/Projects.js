a5cad1540a65dea48c434163b0b4e6b67fd328f4
DylanRhinehart <dylanrhinehart@gmail.com>
2023-06-21 13:46:03 -0700

    fixed carousel buttons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -53,40 +53,42 @@ const projArray = [{
               preventScrollOnSwipe={true}
               itemsToShow={1}
               itemsToScroll={1}
-              // forwardBtnProps={{
-              //   //here you can also pass className, or any other button element attributes
-              //   style: {
-              //     alignSelf: 'center',
-              //     background: 'black',
-              //     border: 'none',
-              //     borderRadius: '50%',
-              //     color: 'white',
-              //     cursor: 'pointer',
-              //     fontSize: '20px',
-              //     height: 30,
-              //     lineHeight: 1,
-              //     textAlign: 'center',
-              //     width: 30
-              //   },
-              //   children: <span>{`>`}</span>,
-              // }}
-              // backwardBtnProps={{
-              //   //here you can also pass className, or any other button element attributes
-              //   style: {
-              //     alignSelf: 'center',
-              //     background: 'black',
-              //     border: 'none',
-              //     borderRadius: '50%',
-              //     color: 'white',
-              //     cursor: 'pointer',
-              //     fontSize: '20px',
-              //     height: 30,
-              //     lineHeight: 1,
-              //     textAlign: 'center',
-              //     width: 30,
-              //   },
-              //   children: <span>{`<`}</span>,
-              // }}
+              forwardBtnProps={{
+                //here you can also pass className, or any other button element attributes
+                style: {
+                  className: 'custom-btn',
+                  alignSelf: 'center',
+                  background: 'black',
+                  border: 'none',
+                  borderRadius: '50%',
+                  color: 'white',
+                  cursor: 'pointer',
+                  fontSize: '20px',
+                  height: 30,
+                  lineHeight: 1,
+                  textAlign: 'center',
+                  width: 30
+                },
+                children: <span>{``}</span>,
+              }}
+              backwardBtnProps={{
+                //here you can also pass className, or any other button element attributes
+                style: {
+                  className: 'custom-btn',
+                  alignSelf: 'center',
+                  background: 'black',
+                  border: 'none',
+                  borderRadius: '50%',
+                  color: 'white',
+                  cursor: 'pointer',
+                  fontSize: '20px',
+                  height: 30,
+                  lineHeight: 1,
+                  textAlign: 'center',
+                  width: 30,
+                },
+                children: <span>{``}</span>,
+              }}
               responsiveProps={[
                 {
                   itemsToShow: 2,
