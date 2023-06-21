e638de6bc6ea4cdb00fa50958fada319c9249827
DylanRhinehart <dylanrhinehart@gmail.com>
2023-06-21 04:17:16 -0700

    fixed css mobile

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,11 +5,11 @@ import { NavLink } from 'react-router-dom';
 export default function Landing() {
 
   return (
-    <div className="p-l-25 flex vp-100 m-col p-25 flip-landing">
-      <div className="flex flex-column center m-uncenter h-100 w-100 m-mar-t-0">
+    <div className="p-l-0 flex vp-100 m-col flip-landing">
+      <div className="flex flex-column center p-l-25 m-uncenter h-100 m-mar-t-0">
         <h1 className="title m-0 p-0 m-title">Dylan<br/>Rhinehart</h1>
         <h2 className="sub-title m-0 p-0 m-job">Software Engineer</h2>
-        <div className="flex flex-row m-col">
+        <div className="flex flex-row">
           <NavLink to="/projects"><button className='btn-1 m-r-10 m-t-10'>Projects</button></NavLink>
           <NavLink to="/about"><button className='btn-2 m-t-10'>About Me</button></NavLink>
         </div>
